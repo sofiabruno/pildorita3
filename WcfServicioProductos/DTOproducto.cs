9d77fe9ae24e5d0c240d0141a3a7567438603e08
sofiabruno <55451343+sofiabruno@users.noreply.github.com>
2020-05-05 20:24:42 -0300

    avances con wfc

---
+++ WcfServicioProductos/DTOproducto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+
+namespace WcfServicioProductos
+{
+    [DataContract]
+    public class DTOproducto
+    {
+        [DataMember]
+        public string Codigo { get; set; }
+
+        [DataMember]
+        public string Nombre { get; set; }
+
+        //[DataMember]
+        //public int Stock { get; set; }
+
+    }
+}
---
+++ WcfServicioProductos/ServicioProductos.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Dominio;
+using Repositorios;
+
+namespace WcfServicioProductos
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioProductos" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione ServicioProductos.svc o ServicioProductos.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ServicioProductos : IServicioProductos
+    {
+      
+        public List<DTOproducto> TraerTodo()
+        {
+            RepoProductos repo = new RepoProductos();
+
+            List<DTOproducto> listProd = new List<DTOproducto>();                     
+          
+            List<Producto> lista = repo.TraerTodos();
+
+            foreach (Producto producto in lista)
+            {
+                DTOproducto produ = new DTOproducto
+                {
+                    Codigo = producto.Codigo,
+                    Nombre = producto.Nombre
+
+                };
+                listProd.Add(produ);
+
+            }
+            return listProd;
+
+
+        }
+    }
+}
