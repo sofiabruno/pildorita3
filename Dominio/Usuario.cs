7dd6572fc6e3a68e42dc6322a2b8f7b1b36dc18b
sofiabruno <55451343+sofiabruno@users.noreply.github.com>
2020-05-13 14:40:44 -0300

    wishes con tuneo rapidode password

--- Dominio/Usuario.cs
+++ Dominio/Usuario.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Dominio
 {
     public class Usuario
     {
         public int Ci { get; set; }
+
+        [Required]
+     
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,50}$", ErrorMessage = "La contraseña debe contar con al menos 6 caracteres, debe incluir al menos una mayúscula, una minúscula y un dígito")]
+
+
         public string Password { get; set; }
         public string Rol { get; set; }
     }
